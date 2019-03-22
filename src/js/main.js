fcecbb5ac99b5d600a3eff418f37d7669d7075c3
Jared Kozak <jkozak@duenorthsystems.org>
2019-03-21 19:10:29 -0500

    Add uptime checks

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,113 @@
-const CLOCK_UPDATE = 500;
 const RELOAD = 60000;
+const CHECK_RATE = 31000;
+const TICK_RATE = 100;
 
-function displayTime() {
-    var d = new Date();
-    var y = document.getElementById("a");
+let hasError = false;
+let lastUptimeCheck;
+
+function tick() {
+    displayTime();
+
+    if (lastUptimeCheck) {
+        const lastChecked = $('#lastChecked');
+        const seconds = Math.ceil((Date.now() - lastUptimeCheck.getTime()) / 1000);
+        lastChecked.text(`last checked ${seconds} seconds ago`);
+        
+    }
+
+    if (hasError) {
+        $('body').addClass('error');
+    } else {
+        $('body').removeClass('error');
+    }
+}
+
+function dateToTime(d) {
     var h = (d.getHours()).toString();
     var m = (d.getMinutes()).toString();
     var s = (d.getSeconds()).toString();
     var h2 = ("0" + h).slice(-2);
     var m2 = ("0" + m).slice(-2);
     var s2 = ("0" + s).slice(-2);
-    y.innerHTML= h2 + ":" + m2 + ":" + s2;
+    return h2 + ":" + m2 + ":" + s2;
+}
+
+function displayTime() {
+    var y = document.getElementById("a");
+    y.innerHTML = dateToTime(new Date());
 }
 
 function doReload() {
     window.location.reload(true);
 }
 
+async function checkServer(server, expectedStatus = 200, port = 443) {
+    return fetch(`https://cors-anywhere.herokuapp.com/${server}:${ port }`)
+        .then((res) => {
+            const okay = res.status === expectedStatus;
+
+            if (!okay) {
+                console.error(res);
+            }
+
+            return okay;
+        })
+        .catch((e) => {
+            console.error(e);
+            return false;
+        })
+}
+
+async function checkServers() {
+    const servers = [
+        'gooil.ca',
+        'www.gooil.ca',
+        'domore.plus',
+        'frc.domore.plus',
+        'shs.domore.plus',
+        'dev.domore.plus',
+        'demo.domore.plus',
+        'fake.domore.plus',
+        'duenorth.systems',
+        // 'duenorthsystems.ca',
+        'duenorthsystems.org',
+        // 'duenorthsystems.com',
+    ];
+
+    const promises = [];
+    for (const server of servers) {
+        promises.push(checkServer(server));
+    }
+
+    result = await Promise.all(promises);
+    console.log(result);
+    
+
+    const uptimes = $('#uptimes');
+    uptimes.empty();
+    hasError = false;
+    for (let i = 0; i < result.length; i++) {
+        const server = servers[i];
+        const okay = result[i];
+
+        const p = $(`<p>${ server } </p>`);
+        if (okay) {
+            p.addClass('okay');
+            p.prepend('✅ ');
+        } else {
+            p.addClass('bad');
+            p.prepend('❌ ');
+            hasError = true;
+        }
+        uptimes.append(p);
+    }
+    lastUptimeCheck = new Date();
+
+}
+
+checkServers();
 displayTime();
-setInterval(displayTime, CLOCK_UPDATE);
+
+setInterval(tick, TICK_RATE);
 setInterval(doReload, RELOAD);
+setInterval(checkServers, CHECK_RATE);
